88793bb03d5ce216becf2c722f070e601d5cc623
beshakimi <w.user2022@gmail.com>
2025-12-20 12:14:16 +0430

    first commit

--- src/components/layout/topbar/NotificationBell.tsx
+++ src/components/layout/topbar/NotificationBell.tsx
@@ -88,7 +88,7 @@ export default function NotificationBell() {
         />
         {unreadNotifications.length > 0 && (
           <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center animate-pulse">
-            <span className="pt-[2px]">
+            <span className="pb-[1px]">
             {unreadNotifications.length}
             </span>
           </div>
